1a203e5e813624fe39fcdeeff3ab11ac66f5f693
Zero2015 <79316040+Zero2015@users.noreply.github.com>
2021-04-30 23:08:44 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# VuePress Ftp
+
+VuePress Ftp 部署插件
+
+## .env
+
+在根目录创建`.env`文件
+
+```shell
+SERVER_HOST=[ip]
+SERVER_PORT=[port]
+SERVER_USER=[user]
+SERVER_PASS=[password]
+SERVER_REMOTE_ROOT=[dir]
+```
+
+## 参数
+
+- SERVER_HOST：Ftp ip 地址
+- SERVER_PORT：Ftp 端口
+- SERVER_USER：Ftp 用户名
+- SERVER_PASS：Ftp 密码
+- SERVER_REMOTE_ROOT： Ftp 远程目录地址
---
+++ index.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv')
+const dotenvExpand = require('dotenv-expand')
+const { build } = require('vuepress')
+const FtpDeploy = require('ftp-deploy')
+const ftpDeploy = new FtpDeploy()
+
+module.exports = (options, ctx) => {
+  const { parsed } = dotenvExpand(dotenv.config())
+  return {
+    name: 'vuepress-plugin-deploy',
+    extendCli: cli => {
+      cli.command('deploy [targetDir]', '').action(() => {
+        build().then(() => {
+          const config = {
+            user: parsed.SERVER_USER || '',
+            password: parsed.SERVER_PASS || '',
+            host: parsed.SERVER_HOST || '127.0.0.1',
+            port: parsed.SERVER_PORT || 21,
+            localRoot: parsed.SERVER_OUTDIR || ctx.outDir,
+            remoteRoot: parsed.SERVER_REMOTE_ROOT || './',
+            include: parsed.SERVER_REMOTE_INCLUDE || ['*', '**/*']
+          }
+          console.log('开始上传...')
+          return ftpDeploy.deploy(config)
+        }).then(res => console.log('finished:', res)).catch(err => console.log(err))
+      })
+    }
+  }
+}
